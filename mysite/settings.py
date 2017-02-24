6e8042338daaac6a2a3bb69ab56597e73f54d674
illsix <hrcho90@zoho.com>
2017-02-25 00:53:24 +0900

    added allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+$3!7#eh$zvdz%_3p^gws9%tqgeoe&%pa368qh*nutsendijmv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'hrcho90.pythonanywhere.com']
 
 
 # Application definition
